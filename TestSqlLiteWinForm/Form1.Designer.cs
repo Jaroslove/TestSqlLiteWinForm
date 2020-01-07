36a3bea13535ef5957e9bd10e41dde6d0aaef542
Jaroslove <jroslovemonro@gmail.com>
2020-01-08 02:12:54 +0300

    init

--- TestSqlLiteWinForm/Form1.Designer.cs
+++ TestSqlLiteWinForm/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.showHistory = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(41, 46);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(41, 163);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.AutoSize = true;
+            this.resultLbl.Location = new System.Drawing.Point(41, 226);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(0, 13);
+            this.resultLbl.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(247, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(409, 193);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // showHistory
+            // 
+            this.showHistory.Location = new System.Drawing.Point(160, 207);
+            this.showHistory.Name = "showHistory";
+            this.showHistory.Size = new System.Drawing.Size(75, 23);
+            this.showHistory.TabIndex = 6;
+            this.showHistory.Text = "button3";
+            this.showHistory.UseVisualStyleBackColor = true;
+            this.showHistory.Click += new System.EventHandler(this.showHistory_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(687, 264);
+            this.Controls.Add(this.showHistory);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resultLbl;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button showHistory;
     }
 }
 
--- TestSqlLiteWinForm/Form1.cs
+++ TestSqlLiteWinForm/Form1.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +16,92 @@ namespace TestSqlLiteWinForm
 {
     public partial class Form1 : Form
     {
+        private const decimal NUMERIC_INCREMENT = 0.001M;
+        private const byte DECIMAL_PLACE = 4;
+        private const string SQL_NAME = "Test.sqlite";
+        private const string SQL_CONNECTION = "Data Source=Test.sqlite";
+
         public Form1()
         {
             InitializeComponent();
+            numericUpDown1.Increment = NUMERIC_INCREMENT;
+            numericUpDown1.DecimalPlaces = DECIMAL_PLACE;
+            numericUpDown2.Increment = NUMERIC_INCREMENT;
+            numericUpDown2.DecimalPlaces = DECIMAL_PLACE;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = Math.Round(numericUpDown1.Value / numericUpDown2.Value, 4);
+
+            resultLbl.Text = result.ToString();
+
+            SQLiteConnection connection = new SQLiteConnection(SQL_CONNECTION);
+            connection.Open();
+            var one = Convert.ToDecimal(numericUpDown1.Value, new CultureInfo("en-US"));
+            var two = Convert.ToDecimal(numericUpDown2.Value, new CultureInfo("en-US"));
+            var three = Convert.ToDecimal(result, new CultureInfo("en-US"));
+
+            string sql = "insert into counter (name, oneInput, twoInput, output)" +
+                         " values ('my name', @one," +
+                         " @two," +
+                         " @three)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, connection);
+            command.Parameters.AddWithValue("@one", one);
+            command.Parameters.AddWithValue("@two", two);
+            command.Parameters.AddWithValue("@three", three);
+
+            command.ExecuteNonQuery();
+
+            connection.Clone();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (!File.Exists(SQL_NAME))
+            {
+                SQLiteConnection.CreateFile(SQL_NAME);
+
+                SQLiteConnection connection = new SQLiteConnection(SQL_CONNECTION);
+
+                connection.Open();
+                string sql = "create table counter (name varchar(20), oneInput decimal, twoInput decimal, output decimal)";
+
+                SQLiteCommand command = new SQLiteCommand(sql, connection);
+
+                command.ExecuteNonQuery();
+
+                connection.Clone();
+            }
+        }
+
+        private void showHistory_Click(object sender, EventArgs e)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            using (var connection = new SQLiteConnection(SQL_CONNECTION))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM counter";
+
+                var command = new SQLiteCommand(sql, connection);
+
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    builder.Append($"name: {reader.GetString(0)}, " +
+                                   $"oneInput: {reader.GetDecimal(1)}, " +
+                                   $"twoInput: {reader.GetDecimal(2)}, " +
+                                   $"result: {reader.GetDecimal(3)}");
+                    builder.AppendLine();
+                }
+            }
+
+            richTextBox1.Text = builder.ToString();
         }
     }
 }
